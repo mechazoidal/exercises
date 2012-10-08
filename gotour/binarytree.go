ffc2d54903a1014e15b1922e144c89eeea61b9c8
Scott Francis <scott@kurokoproject.com>
2012-10-08 09:30:32 -0700

    try implementing Same function

--- gotour/binarytree.go
+++ gotour/binarytree.go
@@ -11,6 +11,7 @@ func Walk(t *tree.Tree, ch chan int) {
 // Is there a left node? if yes, follow it
 // Is there a right node? if yes, follow it
 // No nodes? Push current node value to channel
+	
 	if(t.Left != nil){
 		Walk(t.Left, ch)
 	}
@@ -22,14 +23,26 @@ func Walk(t *tree.Tree, ch chan int) {
 
 // Same determines whether the trees
 // t1 and t2 contain the same values.
-func Same(t1, t2 *tree.Tree) bool
+func Same(t1, t2 *tree.Tree) bool {
+	res1 := make(chan int)
+	res2 := make(chan int)
+	go Walk(t1, res1)
+	go Walk(t2, res2)
+	for i := 0; i <= 10; i++ {
+		if(<-res1 != <- res2){
+			return false
+		}
+	}
+	return true
+}
 
 func main() {
-	ch := make(chan int)
+	/* ch := make(chan int)
 	go Walk(tree.New(1), ch)
-	//result <- ch
 	for i := 0; i < 10; i++ {
 		fmt.Println(<-ch)
 	}
+	*/
+	fmt.Println(Same(tree.New(1), tree.New(1)))
 		
 }
